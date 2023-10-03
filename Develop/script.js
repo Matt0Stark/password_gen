803a256ac2e5de6d9ee805e10868c1adca991be9
Stark <mattstark08@gmail.com>
2023-10-03 09:52:27 -0500

    corrected variables in get char

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ var numberOfCharacters;
 var includeNumbers;
 var includeSpecialCharcters;
 var includeUpperCaseLetters;
-var includeLowerCaseNumbers;
+var includeLowerCaseLetters;
 var finalPassword = "";
 
 // character arrays 
@@ -16,20 +16,47 @@ var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // "final" array
-// var allIncludedCharacters.concat (numbers, specialChar, upperCase, lowerCase)
-
-var allIncludedCharacters = [""];
+var allIncludedCharacters = [];
 
 
+
+//  allIncludedCharacters = [...allIncludedCharacters,...x ];
 
 
 
 function getCharacters (){
-  numberOfCharacters = prompt ("How Many Characters?")
-  includeNumbers = confirm ("Do you want numbers?")
-  includeSpecialCharcters = confirm ("Do you want special characters?")
-  includeUpperCaseLetters = confirm ("Do you want upper case letters?")
-  includeLowerCaseNumbers = confirm ("Do you want lower case letters?")
+
+
+  numberOfCharacters = prompt ("How Many Characters?");
+    // if? 
+
+  includeNumbers = confirm ("Do you want numbers?");
+    if (includeNumbers === true){
+      allIncludedCharacters = [...allIncludedCharacters,...numbers ];
+    }
+  
+
+  includeSpecialCharcters = confirm ("Do you want special characters?");
+    if (includeSpecialCharcters === true){
+      allIncludedCharacters = [...allIncludedCharacters,...specialChar ];
+    }
+  
+
+  includeUpperCaseLetters = confirm ("Do you want upper case letters?");
+    if (includeUpperCaseLetters === true){
+      allIncludedCharacters = [...allIncludedCharacters,...upperCase ];
+    }
+  
+
+  includeLowerCaseLetters = confirm ("Do you want lower case letters?");
+    if (includeLowerCaseLetters === true){
+      allIncludedCharacters = [...allIncludedCharacters,...lowerCase ];
+    }
+  
+console.log (allIncludedCharacters)
+
+//  if (array.length === 0) return 
+  
 }
 
 
@@ -46,6 +73,7 @@ function generatePassword(){
 
   var randoNumber = getARandomNumber()
   finalPassword = finalPassword + randoNumber;
+  // return finalPassword
 } 
 
 // Write password to the #password input (spits it out for me?)
