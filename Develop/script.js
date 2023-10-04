d33a564d7b808e0895ed8985ba93492af59c501a
Stark <mattstark08@gmail.com>
2023-10-03 20:30:23 -0500

    testing for errors and making adjustments

--- Develop/script.js
+++ Develop/script.js
@@ -10,10 +10,10 @@ var includeLowerCaseLetters;
 var finalPassword = "";
 
 // character arrays 
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var specialChar = ["!","@","#","$","%","&","*","?","<",">","/"];
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$", "%", "&", "*", "?", "<", ">", "/"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // "final" array
 var allIncludedCharacters = [];
@@ -24,54 +24,61 @@ var allIncludedCharacters = [];
 
 
 
-function getCharacters (){
+function getCharacters() {
 
+  var inputInvalid = true;
 
-  numberOfCharacters = prompt ("How Many Characters? (Between 8 and 120)");
+  while (inputInvalid) {
+
+    numberOfCharacters = prompt("How Many Characters? (Between 8 and 120)");
     // if? numofchar less that 8 error
-    if (numberOfCharacters < 8){
-      alert ("number of characters must be between 8 and 120")
+    if (numberOfCharacters >= 8 && numberOfCharacters <=120) {
+      inputInvalid = false;
     }
-    // else if numb of character greater than 120 error
-    else if (numberOfCharacters > 120){
-      alert ("number of characters must be between 8 and 120")
+    //
+    else {
+      alert("invalid input, please enter a number between 8 and 120. No letters or special characte")
     }
-    // updates number of characters to answer
+  }
 
 
-    // checks for null
-    if (numberOfCharacters == '0' || numberOfCharacters === '') error (alert)
 
-  includeNumbers = confirm ("Do you want numbers?");
-    if (includeNumbers === true){
-      allIncludedCharacters = [...allIncludedCharacters,...numbers ];
-    }
-  
-  includeSpecialCharcters = confirm ("Do you want special characters?");
-    if (includeSpecialCharcters === true){
-      allIncludedCharacters = [...allIncludedCharacters,...specialChar ];
-    }
-  
-  includeUpperCaseLetters = confirm ("Do you want upper case letters?");
-    if (includeUpperCaseLetters === true){
-      allIncludedCharacters = [...allIncludedCharacters,...upperCase ];
-    }
-  
-  includeLowerCaseLetters = confirm ("Do you want lower case letters?");
-    if (includeLowerCaseLetters === true){
-      allIncludedCharacters = [...allIncludedCharacters,...lowerCase ];
-    }
-  
-  console.log (allIncludedCharacters)
 
-  if (allIncludedCharacters.length === 0){
-    alert ("Pick something idiot")
-    return 
+  // updates number of characters to answer
+
+
+
+
+  includeNumbers = confirm("Do you want numbers?");
+  if (includeNumbers === true) {
+    allIncludedCharacters = [...allIncludedCharacters, ...numbers];
+  }
+
+  includeSpecialCharcters = confirm("Do you want special characters?");
+  if (includeSpecialCharcters === true) {
+    allIncludedCharacters = [...allIncludedCharacters, ...specialChar];
+  }
+
+  includeUpperCaseLetters = confirm("Do you want upper case letters?");
+  if (includeUpperCaseLetters === true) {
+    allIncludedCharacters = [...allIncludedCharacters, ...upperCase];
+  }
+
+  includeLowerCaseLetters = confirm("Do you want lower case letters?");
+  if (includeLowerCaseLetters === true) {
+    allIncludedCharacters = [...allIncludedCharacters, ...lowerCase];
+  }
+
+  console.log(allIncludedCharacters)
+
+  if (allIncludedCharacters.length === 0) {
+    alert("Pick something idiot")
+    return
   }
 }
 
-function getARandomNumber(){
-  for (i=0;i<numberOfCharacters;i++){
+function getARandomNumber() {
+  for (i = 0; i < numberOfCharacters; i++) {
     var index = Math.floor(Math.random() * allIncludedCharacters.length)
     finalPassword += allIncludedCharacters[index];
     console.log(finalPassword)
@@ -81,17 +88,17 @@ function getARandomNumber(){
 }
 
 
-function generatePassword(){
+function generatePassword() {
   getCharacters();
   getARandomNumber();
   return finalPassword
-} 
+}
 
 // Write password to the #password input (spits it out for me?)
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  passwordText.value ="";
+  var password = generatePassword();
   passwordText.value = password;
 
 }
